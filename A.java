7844c97730539981550979ea34a7a1f9cee23faf
emanuelleherranufla <emanuelle.silva@estudante.ufla.br>
2020-06-09 19:14:08 -0300

    primeiro

---
+++ A.java
@@ -0,0 +1,28 @@
+public class A{
+    int A1;
+    float A2;
+
+    public int getA1(){
+        return A1;
+    }
+
+    public float getA2(){
+        return A2;
+    }
+
+    public void setA1(int a){
+        A1 = a;
+    }
+
+    public void setA2(float a){
+        A2 = a;
+    }
+
+    public void MA1(){
+        System.out.println("MA1");
+    }
+
+    public void MA2(){
+        System.out.println("MA2");
+    }
+}
---
+++ B.java
@@ -0,0 +1,28 @@
+public class B{
+    int B1;
+    float B2;
+
+    public int getB1(){
+        return B1;
+    }
+
+    public float getB2(){
+        return B2;
+    }
+
+    public void setB1(int b){
+        B1 = b;
+    }
+
+    public void setB2(float b){
+        B2 = b;
+    }
+
+    public void MB1(){
+        System.out.println("MB1");
+    }
+
+    public void MB2(){
+        System.out.println("MB2");
+    }
+}
