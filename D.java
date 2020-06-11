9b81a4161dfc51a33abf89730f90bf2d05109121
emanuelleherranufla <emanuelle.silva@estudante.ufla.br>
2020-06-11 16:58:21 -0300

    Alterando classe D

--- D.java
+++ D.java
@@ -1,12 +1,12 @@
-public class C {
-    String C1;
-    int C2;
+public class D {
+    String D1;
+    int D2;
 
-    public void MC1(){
-        System.out.println("MC1");
+    public void MD1(){
+        System.out.println("MD1");
     }
 
-    public void MC2(){
-        System.out.println("MC2");
+    public void MD2(){
+        System.out.println("MD2");
     }
 }
